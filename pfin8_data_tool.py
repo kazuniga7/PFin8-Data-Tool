ff0bf885e3d90ca089b3282689cc82e96801cad8
kazuniga7 <kazuniga7@gmail.com>
2026-03-28 13:16:09 -0700

    Add files via upload

--- pfin8_data_tool.py
+++ pfin8_data_tool.py
@@ -376,14 +376,6 @@ def create_chart(chart_data, chart_type, title, x_label, y_label, color_col=None
             )
             fig.update_yaxes(range=[0, 105])
 
-            # Cap bar width when there are 4 or fewer x-axis categories
-            # so bars don't stretch too wide
-            if chart_type in ["Bar Chart", "Grouped Bar Chart", "Stacked Bar Chart"]:
-                n_x_categories = chart_data["x"].nunique()
-                if n_x_categories <= 4:
-                    # Set width equivalent to what 5 bars would look like
-                    fig.update_traces(width=0.5)
-
             # Clean up facet subplot titles (remove "topic=" prefix)
             if facet_col:
                 fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
